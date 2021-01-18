01ada03a1294c3326829bd9b9a5e72fa09371691
parthchauhan35 <parth.chauhan@zycus.com>
2021-01-18 19:56:01 +0530

    added server changes new

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Angular10JwtAuth'`, () => {
+  it(`should have as title 'movie-recommendation'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular10JwtAuth');
+    expect(app.title).toEqual('movie-recommendation');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Angular10JwtAuth app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('movie-recommendation app is running!');
   });
 });
